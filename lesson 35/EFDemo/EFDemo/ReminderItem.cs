bba7cdcb4106293fc0d7ae05765f0bf2f2fcab1b
potap <test@gmaol.com>
2019-12-11 22:22:25 +0300

    ef

---
+++ lesson 35/EFDemo/EFDemo/ReminderItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFDemo
+{
+	public class ReminderItem
+	{
+		public Guid Id { get; set; }
+		public DateTimeOffset Date { get; set; }
+		public string Message { get; set; }
+		public string ContactId { get; set; }
+		public ReminderItemStatus Status { get; set; }
+	}
+}
---
+++ lesson 35/EFDemo/EFDemo/ReminderItemsDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFDemo
+{
+	public class ReminderItemsDbContext : DbContext
+	{
+		private string _connectionString;
+
+		public DbSet<ReminderItem> ReminderItems { get; set; }
+
+		public ReminderItemsDbContext(string connectionString)
+		{
+			_connectionString = connectionString;
+		}
+
+		public ReminderItemsDbContext()
+			: this(@"Data Source=localhost\SQLEXPRESS;Initial Catalog=ReminderEFCore;Integrated Security=true;")
+		{
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(_connectionString);
+		}
+	}
+}
